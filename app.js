ef57396dec465a8fc77542a55ae8c8ec3c54197f
Dnyaneshwar-dev <dnyaneshwar.ware@walchandsangli.ac.in>
2021-10-09 14:32:17 +0530

    added attendance route

--- app.js
+++ app.js
@@ -11,17 +11,42 @@ const prisma = new PrismaClient();
 app.use(cors())
 
 app.use(express.json())
-app.use(express.urlencoded({extended:false}))
+app.use(express.urlencoded({ extended: false }))
 
 app.get('/', async (req, res) => {
-  try{
+  try {
     const users = await prisma.employee.findMany()
     res.json(users)
   }
-  catch(error)
-  {
+  catch (error) {
     res.json(error)
   }
+})
+
+
+
+app.get('/getdata/:employeecode', async (req, res) => {
+  const { employeecode } = req.params
+  console.log(employeecode);
+
+  try {
+    const employee = await prisma.employee.findFirst({
+      where:{
+        employee_code: employeecode
+      }
+    })
+
+    if(!employee){
+      throw "Employee Not Found";
+    }
+
+    res.send({ status:"success", message:"Employee Found", data: employee})
+  }
+  catch(error)
+  {
+    res.send({ status:"error", message:"Employee Not Found", data: error})
+  }
+
 })
 
 
@@ -39,11 +64,11 @@ app.post('/', async (req, res) => {
     })
 
     console.log('Added');
-    res.json({status:"success", message:"Employee Added Successfully",data: user})
+    res.json({ status: "success", message: "Employee Added Successfully", data: user })
   }
   catch (err) {
     console.log(err);
-    res.send({status:"error", message:"Something is Wrong",error:err})
+    res.send({ status: "error", message: "Something is Wrong", error: err })
   }
 })
 
@@ -56,6 +81,16 @@ app.put('/', async (req, res) => {
 app.delete('/:id', async (req, res) => {
 
 })
+
+
+// Adding attendence
+app.post('/attendance',(req,res)=>{
+  console.log(res)
+  res.send("Api Called")
+})
+
+
+
 
 const port = process.env.PORT || 5000;
 
