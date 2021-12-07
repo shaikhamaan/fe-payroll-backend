d7b3833cf170a71b4ec99730cbeee8d7e29ff808
Dnyaneshwar-dev <dnyaneshwar.ware@walchandsangli.ac.in>
2021-12-07 18:07:45 +0530

    Added Telegram Bot

--- TelegramBot/mainbot.js
+++ TelegramBot/mainbot.js
@@ -1,6 +1,6 @@
 import { Telegraf } from 'telegraf'
 
 
-const bot = new Telegraf('')
+const bot = new Telegraf('5022447672:AAGza25M83KJh5uBogzhg54XNHHv7I3s0Dk')
 
 export default bot
--- TelegramBot/notifier.js
+++ TelegramBot/notifier.js
@@ -5,7 +5,7 @@ import getReportByEmployee from '../assets/dailyEmployee.js'
 import moment from 'moment'
 
 // Daily Report Schedular
-const job = schedule.scheduleJob("* * 21 00 * *",async()=>{
+const job = schedule.scheduleJob("* * 22 00 * *",async()=>{
     const d = new Date()
     let subsribers
     try {
@@ -22,10 +22,32 @@ const job = schedule.scheduleJob("* * 21 00 * *",async()=>{
     {
         const employee_code = subsribers[i].employee_code
         const data = await getReportByEmployee(employee_code,date)
-
-        bot.telegram.sendMessage(subsribers[i].telegramid,data)
+        data.date = date
+        bot.telegram.sendMessage(subsribers[i].telegramid,getMessage(data))
     }
     
 }) 
+
+
+
+const getMessage = (data) =>{
+    const message = 
+    `Employee Attendance Report
+     Date: ${data.date}
+     Department : ${data.department}
+     Employee Code: ${data.employee_code}
+     Employee Name: ${data.employee_name}
+     Arrival Time: ${data.arrival_time}
+     Early Time: ${data.early_time}
+     Departure Time: ${data.dept_time}
+     Late Time: ${data.late_time}
+     Over Time: ${data.over_time}
+     Working Hours: ${data.working_hrs}
+     Status: ${data.status}
+     Remarks: ${data.remarks}
+    `
+
+    return message
+}
 
 export default job
