5267d4e6b164eb6eb729338602e2efff480244f3
Dnyaneshwar-dev <dnyaneshwar.ware@walchandsangli.ac.in>
2022-01-31 12:24:05 +0530

    Added pay api

--- routes/payscale.js
+++ routes/payscale.js
@@ -7,6 +7,31 @@ const route = express.Router();
 
 const prisma = new PrismaClient();
 
+route.get("/getpay", async (req, res) => {
+  console.log(req);
+  const { employee_grade, pay_scale_term } = req.query;
+  console.log(pay_scale_term);
+  try {
+    const data = await prisma.payscale.findFirst({
+      where: {
+        employee_grade: employee_grade,
+        pay_scale_term: pay_scale_term,
+      },
+    });
+
+    res.send({
+      ok: true,
+      amount: data.pay_scale,
+    });
+  } catch (error) {
+    console.log(error);
+    res.send({
+      ok: false,
+      amount: 0,
+    });
+  }
+});
+
 route.post("/", async (req, res) => {
   const data = req.body;
 
